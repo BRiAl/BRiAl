573a7cee2fb99702d837852d2f1817138c77053c
dreyer <none@none>
2006-03-09 17:57:32 +0100

    + Initial Version

---
+++ polybori/aux/Skeleton.h
@@ -0,0 +1,36 @@
+// -*- c++ -*-
+//*****************************************************************************
+/** @file 
+ *
+ * @author 
+ * @date 
+ *
+ * 
+ *
+ * @par Copyright:
+ *   (c) by
+ *   Dep. of Mathematics, Kaiserslautern University of Technology and @n
+ *   Fraunhofer Institute for Industrial Mathematics (ITWM)
+ *   D-67663 Kaiserslautern, Germany
+ *
+ * @internal 
+ * @version \$Id$
+ *
+ * @par History:
+ * @verbatim
+ * $Log$
+ * Revision 1.1  2006/03/09 16:57:32  dreyer
+ * + Initial Version
+ *
+ * @endverbatim
+**/
+//*****************************************************************************
+
+// include basic definitions
+#include "pbori_defs.h"
+
+BEGIN_NAMESPACE_PBORI
+
+
+
+END_NAMESPACE_PBORI
