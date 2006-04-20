db0278154e4d0a411a597e0cd2f63a002613026f
bricken <none@none>
2006-04-20 08:48:04 +0200

    initial version

---
+++ groebner/src/groebner_alg.cc
@@ -0,0 +1,11 @@
+/*
+ *  groebner_alg.ccc
+ *  PolyBoRi
+ *
+ *  Created by Michael Brickenstein on 20.04.06.
+ *  Copyright 2006 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "groebner_alg.h"
+
---
+++ groebner/src/groebner_alg.h
@@ -0,0 +1,66 @@
+/*
+ *  groebner_alg.h
+ *  PolyBoRi
+ *
+ *  Created by Michael Brickenstein on 20.04.06.
+ *  Copyright 2006 Mathematisches Forschungsinstitut Oberwolfach. All rights reserved.
+ *
+ */
+
+
+
+#include <polybori.h>
+#include "groebner_defs.h"
+#include "pairs.h"
+#include <boost/dynamic_bitset.hpp>
+#include <vector>
+#ifndef PBORI_GB_ALG_H
+#define PBORI_GB_ALG_H
+
+
+BEGIN_NAMESPACE_PBORIGB
+
+
+
+class PairStatusSet: protected std::vector<boost::dynamic_bitset<> >{
+  bool calculated(int i, int j){
+    if (i>j){
+      int h;
+      h=i;
+      i=j;
+      j=h;
+    }
+    assert(i<j);
+  }
+  void setToCalculated(int i, int j){
+  }
+};
+class PairManager{
+public:
+  PairStatusSet status;
+  std::vector<Pair> queue;
+  
+};
+class GroebnerStrategy{
+public:
+  PairManager pairs;
+  
+  
+
+
+
+
+
+   unsigned int reductionSteps;
+  int normalForms;
+  int currentDegree;
+  
+
+  int easyProductCriterions;
+  int extendedProductCrit;
+  int averageLength;
+
+};
+
+END_NAMESPACE_PBORIGB
+#endif
