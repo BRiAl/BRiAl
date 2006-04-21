0a151f405e33257c588458e569e5db0b4c24cf53
bricken <none@none>
2006-04-21 07:47:19 +0200

    initial version

---
+++ PyPolyBoRi/strategy_wrapper.cc
@@ -0,0 +1,14 @@
+/*
+ *  strategy_wrapper.cc
+ *  PolyBoRi
+ *
+ *  Created by Michael Brickenstein on 21.04.06.
+ *  Copyright 2006 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "strategy_wrapper.h"
+#include <groebner_alg.h>
+void export_strategy(){
+  
+}
---
+++ PyPolyBoRi/strategy_wrapper.h
@@ -0,0 +1,9 @@
+/*
+ *  strategy_wrapper.h
+ *  PolyBoRi
+ *
+ *  Created by Michael Brickenstein on 21.04.06.
+ *  Copyright 2006 __MyCompanyName__. All rights reserved.
+ *
+ */
+void export_strategy();
