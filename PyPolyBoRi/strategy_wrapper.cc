52300973c82ffbf02e9f1c11dcde080fdd466c6c
bricken <none@none>
2006-04-21 08:39:31 +0200

    + introduced GroebnerStrategy

--- PyPolyBoRi/strategy_wrapper.cc
+++ PyPolyBoRi/strategy_wrapper.cc
@@ -8,7 +8,22 @@
  */
 
 #include "strategy_wrapper.h"
+#include <boost/python.hpp>
+
 #include <groebner_alg.h>
+
+//#include <iostream>
+//#include "polybori.h"
+//#include "pbori_defs.h"
+using namespace boost::python;
+using namespace std;
+
+USING_NAMESPACE_PBORIGB
+
 void export_strategy(){
-  
+  boost::python::class_<GroebnerStrategy>("GroebnerStrategy")
+  .def(init<>())
+  .def("addGenerator", &GroebnerStrategy::addGenerator)
+  .def("addGeneratorDelayed", &GroebnerStrategy::addGeneratorDelayed)
+  .def("nextSpoly", &GroebnerStrategy::nextSpoly);
 }
