440cf3082dcc8dd0c6a1e476604a0d84e20257d9
bricken <none@none>
2006-04-25 09:45:20 +0200

    +nf

---
+++ groebner/src/nf.cc
@@ -0,0 +1,11 @@
+/*
+ *  nf.cc
+ *  PolyBoRi
+ *
+ *  Created by Michael Brickenstein on 25.04.06.
+ *  Copyright 2006 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "nf.h"
+
---
+++ groebner/src/nf.h
@@ -0,0 +1,47 @@
+/*
+ *  nf.h
+ *  PolyBoRi
+ *
+ *  Created by Michael Brickenstein on 25.04.06.
+ *  Copyright 2006 __MyCompanyName__. All rights reserved.
+ *
+ */
+#include <algorithm>
+#include "groebner_alg.h"
+#ifndef PBORI_GB_NF_H
+#define PBORI_GB_NF_H
+BEGIN_NAMESPACE_PBORIGB
+class LessWeightedLengthInStrat{
+public:
+  GroebnerStrategy* strat;
+  LessWeightedLengthInStrat(GroebnerStrategy& strat){
+    this->strat=&strat;
+  }
+  bool operator() (const Monomial& a , const Monomial& b){
+    return strat->generators[strat->lm2Index[a]].weightedLength<strat->generators[strat->lm2Index[b]].weightedLength;
+    
+  }
+};
+static int select1(GroebnerStrategy& strat, const Polynomial& p){
+  MonomialSet ms=strat.leadingTerms.intersect(p.lmDivisors());
+  Polynomial workaround =Polynomial(ms);
+  if (ms.emptiness())
+    return -1;
+  else {
+    Monomial min=*(std::min_element(workaround.begin(),workaround.end(), LessWeightedLengthInStrat(strat)));
+    
+    return strat.lm2Index[min];
+  }
+}
+Polynomial nf1(GroebnerStrategy& strat, Polynomial p){
+  //parameter by value, so I can modify it
+  int index;
+  while((index=select1(strat,p))>=0){
+    p=spoly(p,strat.generators[index].p);
+
+  }
+ return p;
+}
+
+END_NAMESPACE_PBORIGB
+#endif
