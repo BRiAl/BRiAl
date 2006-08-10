f63b31f5d5037d2c95de71baa39e66221ff8fd63
bricken <none@none>
2006-08-10 08:39:16 +0200

    *bricken: initial version

---
+++ PyPolyBoRi/out_helper.h
@@ -0,0 +1,15 @@
+#ifndef OUT_HELPER_H
+#define OUT_HELPER_H
+#include <sstream>
+#include <iosfwd>
+#include <boost/python.hpp>
+
+template <class c> boost::python::str streamable_as_str(const c& f)
+{
+  using boost::python::str;
+  std::basic_stringstream<char>  s;
+  s<<f;
+  return boost::python::str(s.str());
+}
+
+#endif
