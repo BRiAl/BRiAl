542d7b106a292ee457d0c15ce2bc6677e5c855b1
dreyer <none@none>
2006-09-20 09:51:43 +0200

    + added use of custom_profile.py

--- PyPolyBoRi/toprofile.py
+++ PyPolyBoRi/toprofile.py
@@ -3,5 +3,13 @@ from sys import path
 from gbrefs import my_import
 #from data import stas1 as data
 from nf import *
-data=my_import("data.uuf75.uuf75_8")
+
+datafilename="data.stas1";
+
+try:
+    from custom_profile import filename as datafilename;
+except:
+    print 'No custom_profile.py file! Using ' + datafilename + '.';
+
+data=my_import(datafilename)
 I=buchberger_C_based2(data.ideal)
