387277ac2faae4790854fbddb085e92836227574
bricken <none@none>
2007-01-16 08:41:12 +0100

    + generate field equations

--- Singular/pbwrapper.h
+++ Singular/pbwrapper.h
@@ -11,14 +11,14 @@
 
 USING_NAMESPACE_PBORI
 USING_NAMESPACE_PBORIGB
-Polynomial sing2pb(poly p){
-  int nvariables=rVar(currRing);
+Polynomial sing2pb(poly p, ring r){
+  int nvariables=rVar(r);
   Polynomial res;
   while(p!=NULL){
     int i;
     Monomial m;
     for(i=1;i<=nvariables;i++){
-      int exponent=pGetExp(p,i);
+      int exponent=p_GetExp(p,i,r);
       if (exponent>0) m=m*Variable(i-1);
     }
     res=res+m;
@@ -26,31 +26,31 @@ Polynomial sing2pb(poly p){
   }
   return res;
 }
-poly pb2sing(Polynomial p){
+poly pb2sing(Polynomial p, ring r){
   Polynomial::exp_iterator it=p.expBegin();
   Polynomial::exp_iterator end=p.expEnd();
-  kBucket_pt bucket=kBucketCreate();
-  number one=nInit(1);
+  kBucket_pt bucket=kBucketCreate(r);
+  number one=n_Init(1,r);
   while(it!=end){
     Exponent e=*it;
-    poly m=pInit();
+    poly m=p_Init(r);
     int i;
     Exponent::const_iterator itit=e.begin();
     Exponent::const_iterator itend=e.end();
     
     while(itit!=itend){
       i=*itit;
-      pSetExp(m,i+1,1);
+      p_SetExp(m,i+1,1,r);
       itit++;
     }
-    pSetCoeff(m,nCopy(one));
-    pSetm(m);
+    p_SetCoeff(m,n_Copy(one,r),r);
+    p_Setm(m,r);
     int len=1;
     kBucket_Add_q(bucket,m,&len);
     
     it++;
   }
-  nDelete(&one);
+  n_Delete(&one,r);
   kBucketCanonicalize(bucket);
   int dummy;
   poly res;
@@ -59,24 +59,79 @@ poly pb2sing(Polynomial p){
   return res;
   
 }
-ideal do_lex_gb(ideal s){
+poly gen_fe(int i,ring r){
+  number one=n_Init(1,r);
+  poly square=p_Init(r);
+  poly lin=p_Init(r);
+  p_SetCoeff(square,n_Copy(one,r),r);
+  p_SetCoeff(lin,n_Copy(one,r),r);
+  p_SetExp(square,i,2,r);
+  p_SetExp(lin,i,1,r);
+  p_Setm(square,r);
+  p_Setm(lin,r);
+  pNext(square)=lin;
+  n_Delete(&one,r);
+  return square;
+}
+
+ideal do_lex_gb(ideal s, bool generate_fe=true){
   int nvariables=rVar(currRing);
   BoolePolyRing pbring(nvariables);
   int i_size=IDELEMS(s);
   int i;
+  
   GroebnerStrategy strat;
   for(i=0;i<i_size;i++){
     poly p=s->m[i];
-    Polynomial pb_p=sing2pb(p);
+    Polynomial pb_p=sing2pb(p,currRing);
     strat.addGeneratorDelayed(pb_p);
   }
   strat.symmGB_F2();
   std::vector<Polynomial> res_pb=strat.minimalizeAndTailReduce();
   i_size=res_pb.size();
-  ideal res=idInit(i_size,0);
-  for(i=0;i<i_size;i++){
-     res->m[i]=pb2sing(res_pb[i]);
+  
+  bool contains_one=false;
+  std::vector<int> fe_needed(nvariables);
+  for(i=0;i<nvariables;i++){
+    fe_needed[i]=true;
   }
+  int fe_n=nvariables;
+  for(i=0;i<i_size;i++){
+    Polynomial p=res_pb[i];
+     assume(!(p.isZero()));
+     Exponent lead=p.leadExp();
+     if (lead.deg()<=1){
+        if (lead.deg()==0){
+          contains_one=true;
+        } else{
+          assume(lead.deg()==1);
+          fe_needed[*lead.begin()]=false;
+          fe_n--;
+        }
+      }
+  }
+  if (contains_one) fe_n=0;
+  if (!(generate_fe)) fe_n=0;
+  ideal res=idInit(i_size+fe_n,0);
+  for(i=0;i<i_size;i++){
+     Polynomial p=res_pb[i];
+     assume(!(p.isZero()));
+     
+     
+     res->m[i]=pb2sing(p,currRing);
+  }
+  int fe_c=0;
+  //todo: minimal field equations
+  if ((!(contains_one))&& (generate_fe)){
+    for(i=1;i<=nvariables;i++){
+      if (fe_needed[i-1]){
+      
+        res->m[i_size+fe_c]=gen_fe(i,currRing);
+        fe_c++;
+      }
+    }
+  }
+  
   return res;
 }
 
