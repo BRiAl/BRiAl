5cd168d5fe62f0e00221d675395678321e370ee5
bricken <none@none>
2007-05-22 11:01:54 +0200

    + command line parsing

--- doc/python/genpythondoc.py
+++ doc/python/genpythondoc.py
@@ -1,9 +1,20 @@
 import sys
 import pydoc
+import os
+def writedoc(thing, forceload=0):
+    """Write HTML documentation to a file in the current directory."""
+    object, name = pydoc.resolve(thing, forceload)
+    page = pydoc.html.page(pydoc.describe(object), pydoc.html.document(object, name))
+    file = open(sys.argv[2], 'w')
+    file.write(page)
+    file.close()
+    print 'wrote', name + '.html'
+
 sys.path.append("PyPolyBoRi")
 sys.path.append("testsuite/py")
-import PyPolyBoRi
-import nf
-import ll
-for m in [PyPolyBoRi,nf,ll]:
-    pydoc.writedoc(m)
+from os import sep
+todoc=sys.argv[1].split(os.sep)[-1].split(".")[-2]
+todoc=__import__(todoc)
+writedoc(todoc)
+
+
