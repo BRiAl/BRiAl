3d5603a2194d00e5e0216d81668df1e33c873f21
dreyer <none@none>
2007-08-09 13:38:19 +0200

    + Initial Version

---
+++ polybori/aux/check_mem.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+$@ &
+
+
+PID=$!
+
+#echo $PID 
+
+VSZ=1
+RSS=1
+VSZnew=1
+RSSnew=1
+
+while [ "$VSZnew" ]
+do
+DATA=`ps -o vsz -o rss -p $PID`
+
+
+if [ $VSZnew -gt $VSZ ]
+    then VSZ=$VSZnew
+fi
+if [ $RSSnew -gt $RSS ]
+    then RSS=$RSSnew
+fi
+sleep 0.1
+#echo $VSZ $RSS
+
+
+VSZnew=`echo $DATA | awk '{print $3}'`
+RSSnew=`echo $DATA | awk '{print $4}'`
+done
+
+
+echo VSZ $VSZ RSS $RSS
