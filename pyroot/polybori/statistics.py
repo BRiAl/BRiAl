f6bc8950ab52b96b1293bc6d8657728ebedf19c3
bricken <none@none>
2007-10-17 15:43:08 +0200

    + alternative implementation

--- pyroot/polybori/statistics.py
+++ pyroot/polybori/statistics.py
@@ -1,6 +1,14 @@
-from polybori.PyPolyBoRi import Monomial,Polynomial
+from polybori.PyPolyBoRi import Monomial,Polynomial,Variable
 def used_vars(l):
     m=Monomial()
     for p in l:
         m=m*Polynomial(p).vars()
+    return m
+def used_vars_set(l):
+    m=Monomial()
+    s=set()
+    for p in l:
+        s.update(Polynomial(p).vars())
+    for v in reversed(list(s)):
+        m=Variable(v)*m
     return m
