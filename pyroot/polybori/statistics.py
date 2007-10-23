722d5696bc120ab90ecf97d453e9f8a25cb3ceaa
bricken <none@none>
2007-10-23 07:57:44 +0200

    + removed dead code/no overloading

--- pyroot/polybori/statistics.py
+++ pyroot/polybori/statistics.py
@@ -1,27 +1,13 @@
 from polybori.PyPolyBoRi import Monomial,Polynomial,Variable
-def used_vars(l):
-    m=Monomial()
-    for p in l:
-        m=m*Polynomial(p).vars()
-    return m
 
-def used_vars(l, bound):
+def used_vars(l, bound=None):
     m=Monomial()
     for p in l:
         m=m*Polynomial(p).vars()
-        if len(m) > bound:
+        if bound and len(m) > bound:
             return m
     return m
 
-def used_vars_set(l):
-    m=Monomial()
-    s=set()
-    for p in l:
-        s.update(Polynomial(p).vars())
-    for v in reversed(list(s)):
-        m=Monomial(Variable(v))*m
-    return m
-    
 
 def used_vars_set(l,bound=None):
     m=Monomial()
