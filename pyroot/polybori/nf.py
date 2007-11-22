8643944052b32e087e9740c299b3f4ede045d152
bricken <none@none>
2007-11-22 13:10:17 +0100

    + removed workaround

--- pyroot/polybori/nf.py
+++ pyroot/polybori/nf.py
@@ -354,7 +354,7 @@ def slimgb(G,deg_bound=1000000000000,over_deg_bound=30, use_faugere=False,use_no
         #res=red.result
         def sort_key(p):
             return p.lead()
-        res_cp=sorted((p for p in res if not p.isZero()),key=sort_key)
+        res_cp=sorted(res, key=sort_key)
         #res_cp=list(res)
         #res_cp.reverse()
         old_ll=strat.llReductor
