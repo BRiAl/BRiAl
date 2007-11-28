49be2cf26d8d995c591201fac3b1d5baf8cc5abf
bricken <none@none>
2007-11-28 07:20:24 +0100

    + std import

---
+++ pyroot/polybori/__init__.py
@@ -0,0 +1,10 @@
+from polybori.PyPolyBoRi import *
+
+
+# Get all-inclusive groebner routine
+from polybori.gbcore import groebner_basis
+
+# Import some high-level modelling functionality
+from polybori.blocks import declare_ring
+from polybori.blocks import HigherOrderBlock, AlternatingBlock, Block
+from polybori.gbrefs import load_file
