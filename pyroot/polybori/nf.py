4df5425d70955d3ae3eecbb16366382143ffce6b
bricken <none@none>
2008-02-08 08:16:46 +0100

    + removed many commented and empty lines of code

--- pyroot/polybori/nf.py
+++ pyroot/polybori/nf.py
@@ -4,26 +4,17 @@ from random import Random
 import copy
 import sys
 from exceptions import NotImplementedError
-#from dijkstra import mydeg
 
-#for g in G:
-#    print hash(g)
-#UNSET="ein drittes"
-#LAZY="REDTAIL"
-#NOREDTAIL="NOREDTAIL"
 
 def myspoly(f,g):
     """only for the profiler to be noticed"""
     return spoly(f,g)
     
-    
-
 
 def lm_tuple(f):
     if isinstance(f,Polynomial):
         f=f.lead()
-    #return f.tuple()
-    
+  
     res=tuple(f)
     
     return res
@@ -43,12 +34,6 @@ def update_cache2(h,cache):
     return (LG,LGM)
 
 
-
-    
-    
-    
-
-
 matrix_prefix="matrix"
 print_matrices=False
 #used_polynomials=list()
@@ -56,10 +41,7 @@ print_matrices=False
 def pkey(p):
     return (p[0],len(p))
 mat_counter=0
-def build_and_print_matrices(v,strat):
-    
-    
-    
+def build_and_print_matrices(v,strat): 
     treated=BooleSet()
     v=list(v)
     rows=0
@@ -78,17 +60,12 @@ def build_and_print_matrices(v,strat):
                     p2=strat[i]
                     p2*=(m/p2.lead())
                     v.append(p2)
-        
-                    #print p2
-          
-            #else:
-            #    print "in treated"
         polys_in_mat.append(p)
         treated=treated.union(p.set())
     m2i=dict([(v,k) for (k,v) in enumerate(BooleSet(treated))])
     polys_in_mat=[[m2i[t] for t in p] for p in polys_in_mat]
     polys_in_mat.sort(key=pkey)
-    #print polys_in_mat
+
     global mat_counter
     mat_counter=mat_counter+1
     import Image, ImageDraw
@@ -97,24 +74,20 @@ def build_and_print_matrices(v,strat):
     cols=len(m2i)
     im=Image.new("1",(cols,rows),"white")
 
-    #draw = ImageDraw.Draw(im)
     for i in xrange(len(polys_in_mat)):
         p=polys_in_mat[i]
         for j in p:
            assert i<rows
            assert j<cols
-           #if mat_counter==1:
-            #print (i,j)
+
            im.putpixel((j,i), 0)
-           
-    #del draw
+
     file_name=matrix_prefix+str(mat_counter)+".png"
     import os
     os.system("rm -f file_name")
     im.save(file_name)
     del im
-    
-    
+
     print "MATRIX_SIZE:", rows,"x",cols
     
    
@@ -145,7 +118,6 @@ def noro_step(polys,strat):
     i2term=list(enumerate(terms))
     term2i=dict(((i,j) for (j,i) in i2term))
     i2term=dict(i2term)
-    #print repr(i2term),repr(term2i)
     rows=len(polys)
     cols=len(terms)
     m=createMatGF2(rows,cols)
@@ -153,19 +125,12 @@ def noro_step(polys,strat):
         for t in BooleSet(p.set()):
             j=term2i[t]
             m[i,j]=1
-    
-    #for i in xrange(len(polys)):
-    #    print "|".join([str(m[i,j]) for j in xrange(len(terms))])
     rank=m.gauss()
     
     polys=[sum_terms([i2term[j] for j in xrange(cols) if m[i,j]]) for i in xrange(rank)]
-    #print polys
+
     return polys
-    #for i in xrange(len(polys)):
-    #    print "|".join([str(m[i,j]) for j in xrange(len(terms))])
-    #print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-    #print "polys:",len(polys),"terms:",len(terms)
-    #print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+
 def build_and_print_matrices_deg_colored(v,strat):
     
     if len(v)==0:
@@ -175,12 +140,11 @@ def build_and_print_matrices_deg_colored(v,strat):
     v=list(v)
     rows=0
     polys_in_mat=[]
-    #v_orig=list(v)
+
     while(len(v)>0):
         rows=rows+1
         p=v[0]
         v=v[1:]
-        #used_polynomials.append(p)
         for m in list(p):
             m=Monomial(m)
             if not m in BooleSet(treated):
@@ -189,11 +153,6 @@ def build_and_print_matrices_deg_colored(v,strat):
                     p2=strat[i]
                     p2*=(m/p2.lead())
                     v.append(p2)
-        
-                    #print p2
-          
-            #else:
-            #    print "in treated"
         polys_in_mat.append(p)
         treated=treated.union(p.set())
     m2i=dict([(v,k) for (k,v) in enumerate(BooleSet(treated))])
@@ -203,7 +162,6 @@ def build_and_print_matrices_deg_colored(v,strat):
     i2deg=dict([(m2i[m],m.deg()) for m in BooleSet(treated)])
     polys_in_mat=[[m2i[t] for t in p] for p in polys_in_mat]
     polys_in_mat.sort(key=pkey)
-    #print polys_in_mat
     global mat_counter
     mat_counter=mat_counter+1
     import Image, ImageDraw, ImageColor
@@ -211,19 +169,12 @@ def build_and_print_matrices_deg_colored(v,strat):
     rows=len(polys_in_mat)
     cols=len(m2i)
     im=Image.new("RGB",(cols,rows),"white")
-
-    #draw = ImageDraw.Draw(im)
     for i in xrange(len(polys_in_mat)):
         p=polys_in_mat[i]
         for j in p:
            assert i<rows
            assert j<cols
-           #if mat_counter==1:
-            #print (i,j)
-           #im.putpixel((j,i), ImageColor.getrgb("hsl(0,"+str((100*i2deg[j])/max_deg)+"%,50%)"))
            im.putpixel((j,i), ImageColor.getrgb("hsl("+str(270-(270*i2deg[j])/max_deg)+",100%,50%)"))
-           
-    #del draw
     file_name=matrix_prefix+str(mat_counter)+".png"
     import os
     os.system("rm -f file_name")
@@ -234,7 +185,6 @@ def build_and_print_matrices_deg_colored(v,strat):
     print "MATRIX_SIZE:", rows,"x",cols   
     
 def symmGB_F2_python(G,deg_bound=1000000000000,over_deg_bound=0, use_faugere=False,use_noro=False,optLazy=True,optRedTail=True, max_growth=2.0, step_factor=1.0, implications=False, prot=False, full_prot=False,selection_size=1000, optExchange=True, optAllowRecursion=False,ll=False,optLinearAlgebraInLastBlock=True):
-    #print implications
     if use_noro:
         raise NotImplementedError
     if use_noro and use_faugere:
@@ -245,7 +195,6 @@ def symmGB_F2_python(G,deg_bound=1000000000000,over_deg_bound=0, use_faugere=Fal
                 print "-"
         else:
             if prot:
-                #print "s"
                 if full_prot:
                     print p
                 print "Result: ", "deg:", p.deg(), "lm: ", p.lead(), "el: ", p.elength()
@@ -257,11 +206,7 @@ def symmGB_F2_python(G,deg_bound=1000000000000,over_deg_bound=0, use_faugere=Fal
     if (isinstance(G,list)):
         if len(G)==0:
             return []
-            
-            
-        #print "gr"
-        G=[Polynomial(g) for g in G]
-        #print "gra"    
+        G=[Polynomial(g) for g in G]  
         strat=GroebnerStrategy()
         strat.optRedTail=optRedTail
         strat.optLazy=optLazy
@@ -270,14 +215,10 @@ def symmGB_F2_python(G,deg_bound=1000000000000,over_deg_bound=0, use_faugere=Fal
         strat.enabledLog=prot
         strat.optLL=ll
         strat.optLinearAlgebraInLastBlock=optLinearAlgebraInLastBlock
-        #strat.addGenerator(G[0])
-        
-        
         strat.redByReduced=False#True
         
 
         for g in  G:
-            #print G[i], G[i].__class__,i,len(G)
             if not g.isZero():
                 strat.addGeneratorDelayed(g)
     else:
@@ -289,9 +230,6 @@ def symmGB_F2_python(G,deg_bound=1000000000000,over_deg_bound=0, use_faugere=Fal
     while strat.npairs()>0:
         i=i+1
         if prot:
-            #pass 
-            #print_ring_info()
-            #ps=strat.someSpolysInNextDegree(1000)
             print "Current Degree:", strat.topSugar()
         if (strat.topSugar()>deg_bound) and (over_deg_bound<=0):
             return strat
@@ -299,7 +237,6 @@ def symmGB_F2_python(G,deg_bound=1000000000000,over_deg_bound=0, use_faugere=Fal
             ps=strat.someSpolysInNextDegree(over_deg_bound)
             over_deg_bound-=len(ps)
         else:
-            #ps=strat.smallSpolysInNextDegree(100.0,selection_size)
             ps=strat.someSpolysInNextDegree(selection_size)
             
         if prot:
@@ -310,21 +247,10 @@ def symmGB_F2_python(G,deg_bound=1000000000000,over_deg_bound=0, use_faugere=Fal
             "EASYP",strat.easyProductCriterions,"EXTP",strat.extendedProductCriterions
             print len(ps), "spolys added"
             
-        #red=SlimgbReduction(strat)
-        #ps=set(ps)
-        #print len(ps), "spolys added"
-        
-        
-        
-        
-        
-        
         if use_noro or use_faugere:
              #res=noro_step(ps,strat)
              v=BoolePolynomialVector()
              for p in ps:
-                    #print p
-                    #p=ll_red_nf(p,strat.llReductor)
                     if not p.isZero():
                         v.append(p)
              if use_noro:
@@ -360,23 +286,6 @@ def symmGB_F2_python(G,deg_bound=1000000000000,over_deg_bound=0, use_faugere=Fal
         #res_cp.reverse()
         old_ll=strat.llReductor
         for p in  res_cp:
-            #if prot:
-            #    print "start redTail"
-            #    print len(p)
-            
-            #p2=nf3_short(strat,p)
-            #if p2.isZero():
-            #    continue
-            #if strat.optRedTail:
-            #   p=red_tail(strat,p)
-               #p=red_tail_short(strat,p)
-           
-            #p=red_tail_self_tuning(strat,p)
-            
-            #p=red_tail_short(strat,p)
-            #if prot:
-            #    print len(p)
-            #    print "end redTail"
             old_len=len(strat)
             add_to_basis(strat,p)
             if implications and old_len==len(strat)-1:
@@ -389,9 +298,7 @@ def symmGB_F2_python(G,deg_bound=1000000000000,over_deg_bound=0, use_faugere=Fal
             if prot:
                 print "(", strat.npairs(), ")"
         new_ll=strat.llReductor
-        #if old_ll!=new_ll:
-        #    strat.llReduceAll()
-        
+
         strat.cleanTopByChainCriterion()
     #strat.toStdOut()
     return strat
@@ -416,9 +323,7 @@ def GPS(G,vars_start, vars_end):
                 #yield trace
             else:
                 branch(strat,trace+[(var,val)],var-1)
-                #workaround because of stack depth
-                #step(strat,trace+[(var,val)],var-1, 0)
-                #step(strat,trace+[(var,val)],var-1, 1)
+
                 
     def branch(strat,trace, var):
         while(strat.variableHasValue(var)):
@@ -462,10 +367,6 @@ def GPS_with_proof_path(G,proof_path, deg_bound,over_deg_bound):
         else:
             if pos>=len(proof_path):
                 print "npairs", strat.npairs()
-                #strat.toStdOut()
-                #l=[p for p in strat]
-                #strat2=symmGB_F2_python(l,prot=True)
-                #strat2.toStdOut()
                 print "minimized:"
                 for p in strat.minimalizeAndTailReduce():
                     print p
@@ -475,9 +376,6 @@ def GPS_with_proof_path(G,proof_path, deg_bound,over_deg_bound):
                 #yield trace
             else:
                 branch(strat,trace+[(pos,val)],proof_path,pos)
-                #workaround because of stack depth
-                #step(strat,trace+[(var,val)],var-1, 0)
-                #step(strat,trace+[(var,val)],var-1, 1)
                 
     def branch(strat,trace, proof_path,pos):
         
@@ -555,31 +453,10 @@ def GPS_with_suggestions(G,deg_bound,over_deg_bound, optLazy=True,optRedTail=Tru
           else:
               first_deg_bound=max(first_deg_bound,g.deg())
               strat.addGeneratorDelayed(g)
-              #d=p.deg()
-              #if d in higher_deg:
-              #    higher_deg[d].append(g)
-              #else:
-              #    higher_deg[d]=[g]
           print g,len(strat)
     else:
       for g in G:
         strat.addAsYouWish(g)
-    #strat.optRedTailDegGrowth=True
-    #for d in sorted(higher_deg.keys()):
-    #
-   # 
-   #     while (strat.npairs()>0) and (strat.topSugar()<d):
-    #        ps=strat.smallSpolysInNextDegree(2.0,100)
-    ##        vec=parallel_reduce(ps,strat,int(1*30),2.0)
-     #       for p in vec:
-     #           strat.addAsYouWish(p)
-     #   vec=BoolePolynomialVector()
-     #   for p in higher_deg[d]:
-     #       vec.append(p)
-     ##   vec=parallel_reduce(vec,strat,int(1*30),2.0)
-      #  for p in vec:
-      #      print p
-      #      strat.addAsYouWish(p)
     if initial_bb:
       strat=symmGB_F2_python(strat,deg_bound=max(deg_bound,first_deg_bound), optLazy=optLazy,over_deg_bound=0,prot=True)
     strat.optLazy=optLazy
