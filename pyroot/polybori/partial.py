ba41d8e72bc9c4acc962d1880b8810a6e66f14f1
bricken <none@none>
2008-04-07 13:17:22 +0200

    + bitwise

--- pyroot/polybori/partial.py
+++ pyroot/polybori/partial.py
@@ -1,11 +1,13 @@
-from polybori import BooleSet
+from polybori import BooleSet,interpolate_smallest_lex
 
 class PartialFunction(object):
     """docstring for PartialFunction"""
     def __init__(self, zeros,ones):
         super(PartialFunction, self).__init__()
-        self.zeros = zeros
-        self.ones = ones
+        self.zeros = zeros.set()
+        self.ones = ones.set()
+    def interpolateSmallestLex(self):
+        return interpolate_smallest_lex(self.zeros,self.ones)
     def __str__(self):
         return "PartialFunction(zeros="+str(self.zeros)+", ones="+str(self.ones)+")"
     def definedOn(self):
@@ -19,3 +21,16 @@ class PartialFunction(object):
         return PartialFunction(zeros,ones)
     def __repr__(self):
         return str(self)
+        
+    def __mul__(self,other):
+        zeros=self.zeros.union(other.zeros)
+        ones=self.ones.intersect(other.ones)
+        return PartialFunction(zeros,ones)
+    def __or__(self,other):
+        zeros=self.zeros.intersect(other.zeros)
+        ones=self.ones.union(other.ones)
+        return PartialFunction(zeros,ones)
+    def __xor__(self,other):
+        return self+other
+    def __and__(self,other):
+        return self*other
