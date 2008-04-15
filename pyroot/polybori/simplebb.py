2386fa81e0041d1faa5161ebf6b4ade6e2d50487
bricken <none@none>
2008-04-15 10:02:33 +0200

    + less_than_n_solutions

--- pyroot/polybori/simplebb.py
+++ pyroot/polybori/simplebb.py
@@ -15,4 +15,25 @@ def buchberger(l):
         if not p.isZero():
             g.addGenerator(p)
     return list(g)
-        
+
+def less_than_n_solutions(ideal,n):
+    l=interred(ideal)
+    g=GroebnerStrategy()
+    all_monomials=Monomial([Variable(i) for i in xrange(number_of_variables())]).divisors()
+    monomials_not_in_leading_ideal=all_monomials
+    for p in l:
+        g.addGenerator(p)
+    while g.npairs()>0:
+        monomials_not_in_leading_ideal=monomials_not_in_leading_ideal%g.minimalLeadingTerms
+        if len(monomials_not_in_leading_ideal)<n:
+            return True
+        g.cleanTopByChainCriterion()
+        p=g.nextSpoly()
+        p=g.nf(p)
+        if not p.isZero():
+            g.addGenerator(p)
+    monomials_not_in_leading_ideal=monomials_not_in_leading_ideal%g.minimalLeadingTerms
+    if len(monomials_not_in_leading_ideal)<n:
+        return True
+    else:
+        return False
