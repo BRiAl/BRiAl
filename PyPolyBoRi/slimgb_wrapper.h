b87a6b2f90f9cd02bdc063b784f067194003f9b5
bricken <none@none>
2008-06-06 10:40:18 +0200

    + fix copyright

--- PyPolyBoRi/slimgb_wrapper.h
+++ PyPolyBoRi/slimgb_wrapper.h
@@ -3,7 +3,7 @@
  *  PolyBoRi
  *
  *  Created by Michael Brickenstein on 02.05.06.
- *  Copyright 2006 The PolyBoRi Team. All rights reserved.
+ *  Copyright 2006 The PolyBoRi Team. See LICENSE file.
  *
  */
 
