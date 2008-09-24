6c5fe9ab3932e613c96abd0e1318c3006d5c2579
bricken <none@none>
2008-09-24 14:24:33 +0200

    + first doctest

--- pyroot/polybori/intersect.py
+++ pyroot/polybori/intersect.py
@@ -12,6 +12,16 @@ from polybori.statistics import used_vars_set
 from itertools import chain
 
 def intersect(i,j,**gb_opts):
+    """
+    This functions intersects two ideals. The first ring variable is used as helper variable for this
+    intersection. It is assumed, that it doesn't occur in the ideals, and that we have an elimination ordering
+    for this variables. Both assumptions are checked.
+    >>> from polybori.PyPolyBoRi import Ring
+    >>> r=Ring(1000)
+    >>> x=Variable
+    >>> intersect([x(1),x(2)+1],[x(1),x(2)])
+    [x(1)]
+    """
     uv=used_vars_set(i)*used_vars_set(j)
     t=Variable(0)
     if uv.reducibleBy(t):
@@ -20,3 +30,10 @@ def intersect(i,j,**gb_opts):
         raise ValueError, "need elimination ordering for first ring variable"
     gb=groebner_basis(list(chain((t*p for p in i),((1+t)*p for p in j))),**gb_opts)
     return [p for p in gb if p.navigation().value()>t.index()]
+
+def _test():
+    import doctest
+    doctest.testmod()
+
+if __name__ == "__main__":
+    _test()
