fa4a06b01bc6fe2324e202a0fabe7a4dc01762ee
bricken <none@none>
2009-02-24 12:08:58 +0100

    + faster implementation of addition networks

--- pyroot/polybori/addition.py
+++ pyroot/polybori/addition.py
@@ -1,8 +1,32 @@
 from polybori.PyPolyBoRi import Polynomial
 from polybori.partial import PartialFunction
 from polybori.specialsets import all_monomials_of_degree_d, power_set
-def add_bits(bits):
+def add_bits_old(bits):
     """Adds n bits
+    >>> from polybori import *
+    >>> r=Ring(10)
+    >>> add_bits_old([Variable(i) for i in xrange(3)])
+    [x(0) + x(1) + x(2), x(0)*x(1) + x(0)*x(2) + x(1)*x(2)]
+    >>> add_bits_old([Variable(i) for i in xrange(4)])
+    [x(0) + x(1) + x(2) + x(3), x(0)*x(1) + x(0)*x(2) + x(0)*x(3) + x(1)*x(2) + x(1)*x(3) + x(2)*x(3)]
+    """
+    bits=list(bits)
+    n=len(bits)
+    deg_d_monomials=[Polynomial(all_monomials_of_degree_d(i, bits)) for i in xrange(n+1)]
+    full=power_set(bits)
+    bits_expr=[]#[sum(bits)]
+    step=0
+    while n>2**step:
+        to_one=sum([deg_d_monomials[i] for i in xrange(n+1) if i & 2**step])
+        to_one=Polynomial(to_one)
+        fun=PartialFunction(ones=to_one, zeros=full.diff(to_one))
+        poly=fun.interpolate_smallest_lex()
+        bits_expr.append(poly)
+        step=step+1
+    return bits_expr
+
+def add_bits(bits):
+    """Adds n bits, by Lucas theorem
     >>> from polybori import *
     >>> r=Ring(10)
     >>> add_bits([Variable(i) for i in xrange(3)])
@@ -12,20 +36,14 @@ def add_bits(bits):
     """
     bits=list(bits)
     n=len(bits)
-    deg_d_monomials=[Polynomial(all_monomials_of_degree_d(i, bits)) for i in xrange(n+1)]
-    full=power_set(bits)
+    
     bits_expr=[]#[sum(bits)]
-    comp=1
     step=0
-    while n>comp:
-        to_one=sum([deg_d_monomials[i] for i in xrange(n+1) if i & 2**step])
-        to_one=Polynomial(to_one)
-        fun=PartialFunction(ones=to_one, zeros=full.diff(to_one))
-        poly=fun.interpolate_smallest_lex()
-        bits_expr.append(poly)
-        comp=comp*2
+    while n>2**step:
+        bits_expr.append(Polynomial(all_monomials_of_degree_d(2**step, bits)))
         step=step+1
     return bits_expr
+
 
 def _test():
     import doctest
