e825b00af826b67e538c8b1f0f20db4b930f86d7
bricken <none@none>
2009-03-10 15:45:28 +0100

    + shorter lines

--- pyroot/polybori/simplebb.py
+++ pyroot/polybori/simplebb.py
@@ -19,12 +19,14 @@ def buchberger(l):
 def less_than_n_solutions(ideal,n):
     l=interred(ideal)
     g=GroebnerStrategy()
-    all_monomials=Monomial([Variable(i) for i in xrange(number_of_variables())]).divisors()
+    all_monomials=Monomial([Variable(i) for i 
+        in xrange(number_of_variables())]).divisors()
     monomials_not_in_leading_ideal=all_monomials
     for p in l:
         g.add_generator(p)
     while g.npairs()>0:
-        monomials_not_in_leading_ideal=monomials_not_in_leading_ideal%g.reduction_strategy.minimal_leading_terms
+        monomials_not_in_leading_ideal=monomials_not_in_leading_ideal\
+            % g.reduction_strategy.minimal_leading_terms
         if len(monomials_not_in_leading_ideal)<n:
             return True
         g.clean_top_by_chain_criterion()
@@ -32,7 +34,8 @@ def less_than_n_solutions(ideal,n):
         p=g.nf(p)
         if not p.is_zero():
             g.add_generator(p)
-    monomials_not_in_leading_ideal=monomials_not_in_leading_ideal%g.reduction_strategy.minimal_leading_terms
+    monomials_not_in_leading_ideal=monomials_not_in_leading_ideal\
+        % g.reduction_strategy.minimal_leading_terms
     if len(monomials_not_in_leading_ideal)<n:
         return True
     else:
