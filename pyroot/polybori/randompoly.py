7e40079812c18a882cf5a0a320481ac1633e029b
bricken <none@none>
2009-09-08 14:25:06 +0200

    + guarantee solution

--- pyroot/polybori/randompoly.py
+++ pyroot/polybori/randompoly.py
@@ -1,4 +1,5 @@
-from polybori.PyPolyBoRi import Monomial, Variable, global_ring, random_set, Polynomial, set_random_seed, Ring
+from polybori.PyPolyBoRi import Monomial, Variable, global_ring, random_set, Polynomial, set_random_seed, Ring, ll_red_nf_redsb
+from polybori.ll import ll_encode
 from random import Random
 from pprint import pprint, pformat
 
@@ -29,16 +30,21 @@ def sparse_random_system(number_of_polynomials, variables_per_polynomial, degree
     The system will be satisfiable by at least one solution.
     >>> from polybori import *
     >>> r=Ring(10)
-    >>> s=sparse_random_system(number_of_polynomials = 5, variables_per_polynomial = 3, degree=2, random_seed=123)
+    >>> s=sparse_random_system(number_of_polynomials = 20, variables_per_polynomial = 3, degree=2, random_seed=123)
     >>> [p.deg() for p in s]
-    [2, 2, 2, 2, 2]
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    >>> groebner_basis(s)
+    [x(0), x(1), x(2), x(3), x(4) + 1, x(5), x(6) + 1, x(7), x(8) + 1, x(9)]
     """
     ring = global_ring()
     if random_seed is not None:
         set_random_seed(random_seed)
     random_generator = Random(random_seed)
-
+    solutions=[]
     variables = [Variable(i) for i in xrange(ring.n_variables())]
+    for v in variables:
+        solutions.append(v+random_generator.randint(0,1))
+    solutions=ll_encode(solutions)
     res = []
     while len(res)<number_of_polynomials:
         variables_as_monomial=Monomial(
@@ -50,13 +56,14 @@ def sparse_random_system(number_of_polynomials, variables_per_polynomial, degree
         p=sum([p.graded_part(i) for i in xrange(degree+1)])
         if p.deg()==degree:
             res.append(p)
+    res=[p+ll_red_nf_redsb(p, solutions) for p in res]# evaluate it to guarantee a solution
     return res
 
 def sparse_random_system_data_file_content(
     number_of_variables, **kwds):
     r"""
     >>> sparse_random_system_data_file_content(10, number_of_polynomials = 5, variables_per_polynomial = 3, degree=2, random_seed=123)
-    'declare_ring(10)\nideal=[\n[x(0)*x(1) + x(0) + x(1)*x(8) + x(1),\n x(2)*x(5) + x(5) + x(6),\n x(1)*x(2) + x(2)*x(3) + x(2),\n x(0)*x(2) + x(0)*x(3) + 1,\n x(0)*x(5) + x(0) + x(5) + x(9)]\n]\n'
+    'declare_ring(10)\nideal=[\n[x(0)*x(3) + x(0) + x(3)*x(9) + x(3),\n x(0)*x(2) + x(2) + x(5),\n x(0)*x(4) + x(4)*x(8) + x(4),\n x(0)*x(1) + x(0)*x(7),\n x(2)*x(5) + x(2) + x(5) + x(9)]\n]\n'
     """
 
     r=Ring(number_of_variables)
