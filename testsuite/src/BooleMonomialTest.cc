3ea2e04f71f7f497173ec29696b71449be7ae5d9
Ket Kalda <ket.kalda@ITWM.fraunhofer.de>
2010-09-30 13:46:24 +0200

    ADD: BooleMonomial tests for logical operators and small methods

--- testsuite/src/BooleMonomialTest.cc
+++ testsuite/src/BooleMonomialTest.cc
@@ -69,6 +69,8 @@ BOOST_AUTO_TEST_CASE(test_constructors) {
 
 BOOST_AUTO_TEST_CASE(test_assigning_operators) {
 
+  BOOST_TEST_MESSAGE( "Assignments..." );
+
   output_test_stream output;
 
   BOOST_TEST_MESSAGE( "*=" );
@@ -82,6 +84,11 @@ BOOST_AUTO_TEST_CASE(test_assigning_operators) {
   BOOST_CHECK(output.is_equal("y*z*w"));
   output << ((monom_type(x*y*z*v*w)/= x)/= v);
   BOOST_CHECK(output.is_equal("y*z*w"));
+  output << (monom_type(x*y*z*v*w)/= monom_type());
+  BOOST_CHECK(output.is_equal("x*y*z*v*w"));
+  BOOST_CHECK_THROW(monom_type()/monom_type(x), PBoRiError);
+  BOOST_CHECK_THROW(monom_type(y)/monom_type(x), PBoRiError);
+  BOOST_CHECK_THROW(monom_type(z*y)/monom_type(x), PBoRiError);
 }
 
 BOOST_AUTO_TEST_CASE(test_properties) {
@@ -95,13 +102,135 @@ BOOST_AUTO_TEST_CASE(test_properties) {
   BOOST_CHECK_EQUAL(monom_type(ring).isConstant(), true);
 }
 
+BOOST_AUTO_TEST_CASE(test_methods) {
+
+  BOOST_TEST_MESSAGE( "Methods..." );
+
+  output_test_stream output;
+  monom_type monom;
+  monom_type monom2;
+
+  BOOST_TEST_MESSAGE( "firstVariable and firstIndex" );
+  BOOST_CHECK(monom_type(v*w*y*z*x).firstVariable() == x);
+  output << monom_type(v*w*y*z*x).firstIndex();
+  BOOST_CHECK(output.is_equal("0"));
+
+  BOOST_TEST_MESSAGE( "popFirst" );
+  monom = monom_type(y*x);
+  output << monom.popFirst();
+  BOOST_CHECK(output.is_equal("y"));
+  BOOST_CHECK(monom == monom_type(y));
+  output << monom.popFirst();
+  BOOST_CHECK(output.is_equal("1"));
+  BOOST_CHECK(monom == monom_type());
+
+  BOOST_TEST_MESSAGE( "deg and size" );
+  monom = monom_type(v*y*x*x*z);
+  output << monom.deg();
+  BOOST_CHECK(output.is_equal("4"));
+  output << monom.size();
+  BOOST_CHECK(output.is_equal("4"));
+  monom = monom_type();
+  output << monom.deg();
+  BOOST_CHECK(output.is_equal("0"));
+  output << monom.size();
+  BOOST_CHECK(output.is_equal("0"));
+  monom = monom_type(x*w);
+  output << monom.deg();
+  BOOST_CHECK(output.is_equal("2"));
+  output << monom.size();
+  BOOST_CHECK(output.is_equal("2"));
+
+  BOOST_TEST_MESSAGE( "exp" );
+  monom = monom_type(x*w*z);
+  output << monom.exp();
+  BOOST_CHECK(output.is_equal("(0, 2, 4)"));
+  monom = monom_type();
+  output << monom.exp();
+  BOOST_CHECK(output.is_equal("()"));
+
+  BOOST_TEST_MESSAGE( "multiples" );
+  monom = monom_type(x*w*z);
+  monom2 = monom_type(x*y*z*v*w);
+  output << monom.multiples(monom2);
+  BOOST_CHECK(output.is_equal("{{x,y,z,v,w}, {x,y,z,w}, {x,z,v,w}, {x,z,w}}"));
+  monom = monom_type();
+  monom2 = monom_type(x*y);
+  output << monom.multiples(monom2);
+  BOOST_CHECK(output.is_equal("{{x,y}, {x}, {y}, {}}"));
+  monom = monom_type();
+  monom2 = monom_type();
+  output << monom.multiples(monom2);
+  BOOST_CHECK(output.is_equal("{{}}"));
+}
+
 BOOST_AUTO_TEST_CASE(test_logical_operators) {
+
+  BOOST_TEST_MESSAGE( "Logical operators..." );
+
+  output_test_stream output;
+  monom_type monom1;
+  monom_type monom2;
+
+  BOOST_TEST_MESSAGE( "== and !=" );
   BOOST_CHECK_EQUAL(monom_type() == monom_type(), true);
   BOOST_CHECK_EQUAL(monom_type() != monom_type(), false);
   BOOST_CHECK_EQUAL(monom_type() == BooleConstant(true), true);
   BOOST_CHECK_EQUAL(monom_type() != BooleConstant(true), false);
   BOOST_CHECK_EQUAL(monom_type(x) == x, true);
   BOOST_CHECK_EQUAL(monom_type(x) != x, false);
+
+  BOOST_TEST_MESSAGE( "compare as method of one monomial" );
+
+  monom1 = monom_type();
+  monom2 = monom_type(x*y*z*v*w);
+  output << monom1.compare(monom2);
+  BOOST_CHECK(output.is_equal("-1"));
+
+  monom1 = monom_type(x);
+  monom2 = monom_type(x*y);
+  output << monom1.compare(monom2);
+  BOOST_CHECK(output.is_equal("-1"));
+
+  monom1 = monom_type(x*y*z);
+  monom2 = monom_type(x*z);
+  output << monom1.compare(monom2);
+  BOOST_CHECK(output.is_equal("1"));
+
+  monom1 = monom_type(x*y*z);
+  monom2 = monom_type(x*y*z);
+  output << monom1.compare(monom2);
+  BOOST_CHECK(output.is_equal("0"));
+
+  BOOST_TEST_MESSAGE( ">, <, >= and <=" );
+
+  monom1 = monom_type();
+  monom2 = monom_type(x*y*z*v*w);
+  BOOST_CHECK_EQUAL(monom1 < monom2, true);
+  BOOST_CHECK_EQUAL(monom1 <= monom2, true);
+  BOOST_CHECK_EQUAL(monom1 > monom2, false);
+  BOOST_CHECK_EQUAL(monom1 >= monom2, false);
+
+  monom1 = monom_type(x);
+  monom2 = monom_type(x*y);
+  BOOST_CHECK_EQUAL(monom1 < monom2, true);
+  BOOST_CHECK_EQUAL(monom1 <= monom2, true);
+  BOOST_CHECK_EQUAL(monom1 > monom2, false);
+  BOOST_CHECK_EQUAL(monom1 >= monom2, false);
+
+  monom1 = monom_type(x*y*z);
+  monom2 = monom_type(x*z);
+  BOOST_CHECK_EQUAL(monom1 < monom2, false);
+  BOOST_CHECK_EQUAL(monom1 <= monom2, false);
+  BOOST_CHECK_EQUAL(monom1 > monom2, true);
+  BOOST_CHECK_EQUAL(monom1 >= monom2, true);
+
+  monom1 = monom_type(x*y*z);
+  monom2 = monom_type(x*y*z);
+  BOOST_CHECK_EQUAL(monom1 < monom2, false);
+  BOOST_CHECK_EQUAL(monom1 <= monom2, true);
+  BOOST_CHECK_EQUAL(monom1 > monom2, false);
+  BOOST_CHECK_EQUAL(monom1 >= monom2, true);
 }
 
 BOOST_AUTO_TEST_CASE(test_LCM) {
