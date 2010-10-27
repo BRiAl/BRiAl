aeaad0586d1fce423a8904882d7f0bdadffd671e
Ket Kalda <ket.kalda@ITWM.fraunhofer.de>
2010-10-27 11:11:37 +0200

    ADD: BoolePolyRingTest::test_variables

--- testsuite/src/BoolePolyRingTest.cc
+++ testsuite/src/BoolePolyRingTest.cc
@@ -47,5 +47,79 @@ BOOST_AUTO_TEST_CASE(test_constructors) {
   BOOST_TEST_MESSAGE( "Constant rings..." );
 }
 
+BOOST_AUTO_TEST_CASE(test_variables) {
+
+  ring_type ring(3, 0, false);
+  ring_type empty(0);
+  ring.activate();
+  ring_type defaultr;
+
+  BOOST_TEST_MESSAGE( "nVariables, getVariableName, setVariableName" );
+
+  BOOST_CHECK_EQUAL(ring.getVariableName(-1), "UNDEF");
+  BOOST_CHECK_EQUAL(ring.getVariableName(0), "x(0)");
+  BOOST_CHECK_EQUAL(ring.getVariableName(1), "x(1)");
+  BOOST_CHECK_EQUAL(ring.getVariableName(2), "x(2)");
+  BOOST_CHECK_EQUAL(ring.getVariableName(3), "UNDEF");
+  BOOST_CHECK_EQUAL(ring.nVariables(), 3);
+  BOOST_CHECK_EQUAL(empty.getVariableName(-1), "UNDEF");
+  BOOST_CHECK_EQUAL(empty.getVariableName(0), "UNDEF");
+  BOOST_CHECK_EQUAL(empty.getVariableName(1), "UNDEF");
+  BOOST_CHECK_EQUAL(empty.nVariables(), 0);
+  BOOST_CHECK_EQUAL(defaultr.getVariableName(-1), ring.getVariableName(-1));
+  BOOST_CHECK_EQUAL(defaultr.getVariableName(0), ring.getVariableName(0));
+  BOOST_CHECK_EQUAL(defaultr.getVariableName(1), ring.getVariableName(1));
+  BOOST_CHECK_EQUAL(defaultr.getVariableName(2), ring.getVariableName(2));
+  BOOST_CHECK_EQUAL(defaultr.getVariableName(3), ring.getVariableName(3));
+  BOOST_CHECK_EQUAL(defaultr.nVariables(), ring.nVariables());
+
+  ring.setVariableName(0, "x");
+  defaultr.setVariableName(1, "y");
+  ring.setVariableName(3, "v"); // Unwanted behaviour?
+  //defaultr.setVariableName(-1, "w"); //memory access violation at address: 0x00000039
+  empty.setVariableName(0, "x"); // Unwanted behaviour?
+  empty.setVariableName(1, "y"); // Unwanted behaviour?
+  //empty.setVariableName(-1, "z");//memory access violation at address: 0x00000019
+  BOOST_CHECK_EQUAL(ring.getVariableName(-1), "UNDEF");
+  BOOST_CHECK_EQUAL(ring.getVariableName(0), "x");
+  BOOST_CHECK_EQUAL(ring.getVariableName(1), "y");
+  BOOST_CHECK_EQUAL(ring.getVariableName(2), "x(2)");
+  BOOST_CHECK_EQUAL(ring.getVariableName(3), "v");
+  BOOST_CHECK_EQUAL(ring.nVariables(), 3);
+  BOOST_CHECK_EQUAL(defaultr.getVariableName(-1), ring.getVariableName(-1));
+  BOOST_CHECK_EQUAL(defaultr.getVariableName(0), ring.getVariableName(0));
+  BOOST_CHECK_EQUAL(defaultr.getVariableName(1), ring.getVariableName(1));
+  BOOST_CHECK_EQUAL(defaultr.getVariableName(2), ring.getVariableName(2));
+  BOOST_CHECK_EQUAL(defaultr.getVariableName(3), ring.getVariableName(3));
+  BOOST_CHECK_EQUAL(defaultr.nVariables(), ring.nVariables());
+  BOOST_CHECK_EQUAL(empty.getVariableName(-1), "UNDEF");
+  BOOST_CHECK_EQUAL(empty.getVariableName(0), "x");
+  BOOST_CHECK_EQUAL(empty.getVariableName(1), "y");
+  BOOST_CHECK_EQUAL(empty.nVariables(), 0);
+
+  defaultr.setVariableName(0, "y");
+  ring.setVariableName(3, "-y");
+  ring.setVariableName(2, "*");
+  defaultr.setVariableName(4, "ö");
+  empty.setVariableName(0, "variable");
+  BOOST_CHECK_EQUAL(ring.getVariableName(-1), "UNDEF");
+  BOOST_CHECK_EQUAL(ring.getVariableName(0), "y");
+  BOOST_CHECK_EQUAL(ring.getVariableName(1), "y");
+  BOOST_CHECK_EQUAL(ring.getVariableName(2), "*");
+  BOOST_CHECK_EQUAL(ring.getVariableName(3), "-y");
+  BOOST_CHECK_EQUAL(ring.getVariableName(4), "ö");
+  BOOST_CHECK_EQUAL(ring.nVariables(), 3);
+  BOOST_CHECK_EQUAL(defaultr.getVariableName(-1), ring.getVariableName(-1));
+  BOOST_CHECK_EQUAL(defaultr.getVariableName(0), ring.getVariableName(0));
+  BOOST_CHECK_EQUAL(defaultr.getVariableName(1), ring.getVariableName(1));
+  BOOST_CHECK_EQUAL(defaultr.getVariableName(2), ring.getVariableName(2));
+  BOOST_CHECK_EQUAL(defaultr.getVariableName(3), ring.getVariableName(3));
+  BOOST_CHECK_EQUAL(defaultr.getVariableName(4), ring.getVariableName(4));
+  BOOST_CHECK_EQUAL(defaultr.nVariables(), ring.nVariables());
+  BOOST_CHECK_EQUAL(empty.getVariableName(-1), "UNDEF");
+  BOOST_CHECK_EQUAL(empty.getVariableName(0), "variable");
+  BOOST_CHECK_EQUAL(empty.getVariableName(1), "y");
+  BOOST_CHECK_EQUAL(empty.nVariables(), 0);
+}
 
 BOOST_AUTO_TEST_SUITE_END()
