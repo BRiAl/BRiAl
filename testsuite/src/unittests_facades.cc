347b263d5bb43889f7e38537118759f36e66a199
Ket Kalda <ket.kalda@ITWM.fraunhofer.de>
2010-11-30 16:57:23 +0100

    ADD: unittests_facades

---
+++ testsuite/src/unittests_facades.cc
@@ -0,0 +1,19 @@
+// -*- c++ -*-
+//*****************************************************************************
+/** @file unittests_facades.cc
+ *
+ * @author Ket Shcherbakova, Alexander Dreyer
+ * @date 2010-11-30
+ *
+ * boost/test-driven unit test
+ * 
+ * @par Copyright:
+ *   (c) 2010 by The PolyBoRi Team
+ *
+ **/
+//*****************************************************************************
+#define BOOST_TEST_MODULE UnitTests
+
+#include <boost/test/unit_test.hpp>
+
+// Main(), etc automatically generated
