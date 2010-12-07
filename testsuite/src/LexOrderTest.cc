e1dd6ab755b785019e2db99ccf098486446604b8
Ket Kalda <ket.kalda@ITWM.fraunhofer.de>
2010-12-07 12:16:26 +0100

    ADD: LexOrderTests::test_compare

--- testsuite/src/LexOrderTest.cc
+++ testsuite/src/LexOrderTest.cc
@@ -24,10 +24,24 @@ USING_NAMESPACE_PBORI
 
 struct Flex {
   typedef LexOrder order_type;
-  Flex(): ring(5) { BOOST_TEST_MESSAGE( "setup fixture" ); }
+  Flex():
+    ring(5) {
+      x = BooleVariable(0);
+      y = BooleVariable(1);
+      z = BooleVariable(2);
+      v = BooleVariable(3);
+      w = BooleVariable(4);
+      BOOST_TEST_MESSAGE( "setup fixture" );
+      ring.setVariableName(0, "x");
+      ring.setVariableName(1, "y");
+      ring.setVariableName(2, "z");
+      ring.setVariableName(3, "v");
+      ring.setVariableName(4, "w");
+  }
   ~Flex() { BOOST_TEST_MESSAGE( "teardown fixture" ); }
 
   BoolePolyRing ring;
+  BooleVariable x, y, z, v, w;
 };
 
 BOOST_FIXTURE_TEST_SUITE(LexOrderTestSuite, Flex )
@@ -59,6 +73,40 @@ BOOST_AUTO_TEST_CASE(test_getters) {
   BOOST_TEST_MESSAGE( "getOrderCode, getBaseOrderCode" );
   BOOST_CHECK_EQUAL(order.getOrderCode(), COrderEnums::lp);
   BOOST_CHECK_EQUAL(order.getBaseOrderCode(), COrderEnums::lp);
+}
+
+BOOST_AUTO_TEST_CASE(test_compare) {
+
+  order_type order;
+  BOOST_TEST_MESSAGE( "compare" );
+  BooleMonomial monom1 = x;
+  BooleMonomial monom2 = x*x;
+  BOOST_CHECK(order.compare(monom1, monom2) == CTypes::equality);
+  monom1 = y*z*v;
+  monom2 = x*y;
+  BOOST_CHECK(order.compare(monom1, monom2) == CTypes::less_than);
+  monom1 = x*y*z;
+  monom2 = x*y;
+  BOOST_CHECK(order.compare(monom1, monom2) == CTypes::greater_than);
+  monom1 = BooleMonomial();
+  monom2 = w;
+  BOOST_CHECK(order.compare(monom1, monom2) == CTypes::less_than);
+  monom1 = BooleMonomial();
+  monom2 = BooleMonomial();
+  BOOST_CHECK(order.compare(monom1, monom2) == CTypes::equality);
+  BooleExponent exp1(x);
+  BooleExponent exp2(x*x);
+  BOOST_CHECK(order.compare(exp1, exp2) == CTypes::equality);
+  exp1 = BooleExponent(w*x);
+  exp2 = BooleExponent(v*x);
+  BOOST_CHECK(order.compare(exp1, exp2) == CTypes::less_than);
+  exp1 = BooleExponent(x*y*z*v*w);
+  exp2 = BooleExponent(x*y*z*v);
+  BOOST_CHECK(order.compare(exp1, exp2) == CTypes::greater_than);
+  BOOST_CHECK(order.compare(0,1) == CTypes::greater_than);
+  BOOST_CHECK(order.compare(2,1) == CTypes::less_than);
+  BOOST_CHECK(order.compare(-1,-1) == CTypes::equality);
+  BOOST_CHECK(order.compare(1,-1) == CTypes::greater_than);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
