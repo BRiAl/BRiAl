422e41b98f611d649bf0b267ce42b3c373be677a
Alexander Dreyer <adreyer@gmx.de>
2011-05-13 15:36:04 +0200

    FIX: always return in non-void function

--- groebner/src/literal_factorization.cc
+++ groebner/src/literal_factorization.cc
@@ -8,8 +8,10 @@
  */
 
 #include <polybori/groebner/literal_factorization.h>
-#include <iostream>
 #include <polybori/cache/CCacheManagement.h>
+#include <iostream>
+#include <exception>
+
 BEGIN_NAMESPACE_PBORIGB
 
 std::vector<Polynomial> easy_linear_factors(const Polynomial &p){
@@ -415,24 +417,26 @@ LiteralFactorization::const_iterator LiteralFactorization::end(){
     return LiteralFactorizationIterator(this, factors.end(), var2var_map.end());
 }
 LiteralFactorizationIterator::self& LiteralFactorizationIterator::operator++(){
-    if (var2const_iter!=literal_factorization->factors.end()){
-        ++var2const_iter;
-        return *this;
-          }
-          if (var2var_iter!=literal_factorization->var2var_map.end()){
-              ++var2var_iter;
-              return *this;
-          }
+  if (var2const_iter!=literal_factorization->factors.end()){
+    ++var2const_iter;
+  }
+  else if (var2var_iter!=literal_factorization->var2var_map.end()){
+    ++var2var_iter;
+  }
+  return *this;
 }
+
 Polynomial LiteralFactorizationIterator::operator*() const{
-    if (var2const_iter!=literal_factorization->factors.end()){
-      return Variable(var2const_iter->first, m_ring)
-        + Polynomial(var2const_iter->second, m_ring);
-       }
-       if (var2var_iter!=literal_factorization->var2var_map.end()){
-         return Variable(var2var_iter->first, m_ring)
-           + Variable(var2var_iter->second,m_ring);
-       }
+  if (var2const_iter!=literal_factorization->factors.end()){
+    return Variable(var2const_iter->first, m_ring)
+      + Polynomial(var2const_iter->second, m_ring);
+  }
+  if (var2var_iter!=literal_factorization->var2var_map.end()){
+    return Variable(var2var_iter->first, m_ring)
+      + Variable(var2var_iter->second,m_ring);
+  }
+  throw std::runtime_error("Should never reach here!");
+  return m_ring.zero();
 }
 
 END_NAMESPACE_PBORIGB
