bb41628bdfd8b84b7e066b64eaab53a1b4345339
Alexander Dreyer <adreyer@gmx.de>
2012-01-27 10:52:38 +0100

    ADD: doctests

--- pyroot/polybori/easy_polynomials.py
+++ pyroot/polybori/easy_polynomials.py
@@ -2,6 +2,16 @@ from polybori.interpolate import variety_lex_leading_terms, nf_lex_points
 from polybori.PyPolyBoRi import easy_linear_factors
 
 def easy_linear_polynomials(p):
+    """ Get linear polynomials implied by given polynomial.
+    
+    >>> from polybori.frontend import *
+    >>> easy_linear_polynomials(x(1)*x(2) + 1)
+    [x(1) + 1, x(2) + 1]
+    >>> easy_linear_polynomials(x(1)*x(2) + 0)
+    []
+    >>> easy_linear_polynomials(x(0)*x(1) + x(0)*x(2) + 1)
+    [x(0) + 1, x(1) + x(2) + 1]
+    """
     res=[]
     if p.deg()>=2:
         if p.vars_as_monomial().deg() >8: 
@@ -11,7 +21,19 @@ def easy_linear_polynomials(p):
         else:
             res= easy_linear_polynomials_via_interpolation(p)
     return res
+
 def easy_linear_polynomials_via_interpolation(p):
+    """ Get linear polynomials implied by given polynomial using interpolation
+    of the variety.
+    
+    >>> from polybori.frontend import *
+    >>> easy_linear_polynomials_via_interpolation(x(1)*x(2) + 1)
+    [x(1) + 1, x(2) + 1]
+    >>> easy_linear_polynomials_via_interpolation(x(1)*x(2) + 0)
+    []
+    >>> easy_linear_polynomials_via_interpolation(x(0)*x(1) + x(0)*x(2) + 1)
+    [x(0) + 1, x(1) + x(2) + 1]
+    """
     res = []
     p_vars = p.vars_as_monomial()
     space = p_vars.divisors()
