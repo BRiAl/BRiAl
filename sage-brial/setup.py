4bd15e4ba3301ee0fffbdf35437883175ab2c52b
François Bissey <frp.bissey@gmail.com>
2017-11-27 22:15:33 +1300

    bump sage-brial and brial version numbers for minor release

--- sage-brial/setup.py
+++ sage-brial/setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup( name="brial"
-     , version="1.0.1"
+     , version="1.0.2"
      , description="Python implementation of BRiAl"
      , long_description="BRiAl is the legacy implementation of PolyBoRi"
      , url="https://github.com/BRiAl/pyBRiAl"
